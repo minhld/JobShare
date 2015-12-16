47e442180b8c3f3959f40140b5e306a4d71887c1
Le Dinh Minh <ledinhminh3883@gmail.com>
2015-12-15 17:55:32 -0800

    finalize the API

---
+++ app/src/main/java/com/minhld/supports/PeerSpecs.java
@@ -0,0 +1,12 @@
+package com.minhld.supports;
+
+/**
+ * Created by minhld on 12/7/2015.
+ */
+public class PeerSpecs {
+    public long totalCPU;
+    public long availableCPU;
+    public long totalMemory;
+    public long availableMemory;
+    public long point;
+}
